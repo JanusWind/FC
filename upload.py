35c0f01b3252cb69f23954c3a21a19a709ddeed2
Ramiz Qudsi <ahmadr@shadowfax.physics.udel.edu>
2017-11-06 21:05:39 -0500

    modified:   .gitignore

--- upload.py
+++ upload.py
@@ -1,11 +1,5 @@
 # Lists all the git commands which are required to push the changes made.
 
-c= None
-
 git add --a
 git commit -a
-c = git commit -a
-if ( c is None ) :
-	continue
-else :
-	git push
+git push
