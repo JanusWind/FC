768aa6b3c154eae42fc4e9e6cc302c52fa86b4a0
Ramiz Qudsi <ahmadr@shadowfax.physics.udel.edu>
2017-11-07 16:59:21 -0500

    modified:   .gitignore 	deleted:    test.sh 	deleted:    upload.py

--- test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "hello, $USER. I wish to list some files of yours"
-echo "listing files in the current directory, $PWD"
-ls  # list files
--- upload.py
+++
@@ -1,5 +0,0 @@
-# Lists all the git commands required to push the changes to online repository.
-
-git add --a
-git commit -a
-git push
