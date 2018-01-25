cbf75dee360c972d43e3500ff4740372762ab003
Mark Pultrone <pultrone@ubuntu>
2018-01-25 17:29:14 -0500

    modified:   janus_pl_spec.py

--- janus_pl_spec.py
+++ janus_pl_spec.py
@@ -44,48 +44,70 @@ class pl_spec( ) :
 	              azim_cen=None, phi_del=None, volt_cen=None, volt_del=None,
 	              psd=None, rot=3.                                       ) :
 
-		self._n_bin     = 5 #TODO Confirm
+		self._n_bin     = 14 #TODO Confirm
 		self._n_the     = 5
-		self._n_phi     = 14 #TODO Confirm this
+		self._n_phi     = 5 #TODO Confirm this
 		self._n_bin     = n_bin
 		self._t_strt    = t_strt
 		self._t_stop    = t_stop
 		self._rot       = t_stop - t_strt
 
 		if ( azim_cen == None ) :
-			azim_cen = [ None for p in range( self._n_phi ) ]
+			azim_cen = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
+		else:
+			azim_cen = adjust(azim_cen)
 
 		if ( phi_del == None ) :
-			phi_del = [ None for p in range( self._n_phi ) ]
+			phi_del  = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
+		else:
+			phi_del = adjust(phi_del)
 
 		if ( elev_cen == None ) :
-			elev_cen = [ None for t in range( self._n_the )    ]
+			elev_cen = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
 
 		if ( the_del == None ) :
-			the_del = [ None for t in range( self._n_the )    ]
+			the_del  = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
 
 		if ( volt_cen == None ) :
-			volt_cen = [ [ None for b in range( self._n_bin )   ]	
+			volt_cen = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
+		else:
+			volt_cen = adjust(volt_cen)	
 
 		if ( volt_del == None ) :
-			volt_del = [ [ None for b in range( self._n_bin )   ]	
+			volt_del = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
+		else:
+			volt_del = adjust(volt_del)	
 
 		if ( psd == None ) :
-			psd = [ [ [ None for b in range( self._n_bin ) ]
-			                 for t in range( self._n_the ) ]
-			                 for p in range( self._n_phi ) ]
+			psd      = [ [ [ None for b in range( self._n_bin ) ]
+			                      for p in range( self._n_phi ) ]
+			                      for t in range( self._n_the ) ]
+		else:
+			psd = adjust(psd)
 
 		self.arr = [[[ pl_dat( spec=self,
-		                       azim_cen = azim_cen[p],
-		                       phi_del  = phi_del[p],
-		                       elev_cen = elev_cen[t],
-		                       the_del  = the_del[t],
-		                       volt_cen = volt_cen[b] 
-		                       volt_del = volt_del[b], 
-		                       psd = psd[b][t][p]      ) 
-		                   for b in range(self._n_bin) ]
-		                   for t in range(self._n_the) ]
-		                   for p in range(self._n_phi) ]
+		                       azim_cen = azim_cen[b][p][t],
+		                       phi_del  = phi_del[b][p][t],
+		                       elev_cen = elev_cen[b][p][t],
+		                       the_del  = the_del[b][p][t],
+		                       volt_cen = volt_cen[b][p][t] 
+		                       volt_del = volt_del[b][p][t], 
+		                       psd = psd[b][p][t]            ) 
+		                              for b in range( self._n_bin ) ]
+		                              for t in range( self._n_phi ) ]
+		                              for p in range( self._n_the ) ]
 
 #		self.set_rot( rot )
 
@@ -97,6 +119,23 @@ class pl_spec( ) :
 		# 'bin' is the voltage sweep number,
 		# 'the' specifies theta of the look direction, and
 		# 'phi' specifies phi of the look direction
+
+	def reverse(matrix) :
+
+	# This function 'adjust':
+	# 1. takes data whose phi values and voltages are reversed and returns
+	# them to their proper order (it does not affect theta values) and
+	# 2. breaks 25 directions into 5 sets of 5 pairs of (theta, phi)
+
+		new_matrix = [ [ [ None for b in range( self._n_bin ) ]
+		                        for p in range( self._n_phi ) ]
+		                        for t in range( self._n_the ) ]
+		for t in range(5):
+			for p in range(5):
+				for b in range(14):
+					new_matrix[-t-1][p][b] =
+					    matrix[-(5*t+p)-1][-b-1]
+		return new_matrix
 
 	def __getitem__(self, key ) : #TODO not yet finished
 
