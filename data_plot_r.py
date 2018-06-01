dd4b4ba618a245ec8667f34d5ead186cda47a4fe
Ramiz Qudsi <ahmadr@shadowfax.physics.udel.edu>
2018-06-01 16:10:02 -0400

    modified:   data_plot_r.py 	modified:   fv_delb_fz01.eps

--- data_plot_r.py
+++ data_plot_r.py
@@ -5,30 +5,39 @@
 
 plt.figure( )
 
-rcParams['figure.figsize'] = 15, 15
+rcParams['figure.figsize'] = 10, 10
 
-xx = [0]*len(dat1_db)
+indx = linspace( -0.08, 0.08, 10 )
+
+xx = [0]*len( indx )
 
 plt.errorbar( dat1_db[0:-1], dat1_s_fv[0:-1], yerr=dat1_s_sig_fv_p[0:-1],
                                                            fmt='o', ecolor='g' )
 plt.plot( ind1, y1_fit_dat, color='r' )
 plt.plot( ind1, y1_fit_thr, dashes=[1,1], color='m' )
 plt.scatter( dat1_db[0:-1], dat1_thr, marker='*', color='r')
-plt.plot( dat1_db, xx, color='gray' )
-plt.plot( xx, dat1_db, color='gray' )
+plt.plot( indx, xx, color='gray' )
+plt.plot( xx, indx, color='gray' )
 
-plt.xticks([0, 0.015, 0.03, 0.045, 0.06, 0.075], fontsize=20)
-plt.yticks([0, 0.015, 0.03, 0.045, 0.06, 0.075], fontsize=20)
+plt.ylim( [ -0.005, 0.08 ] )
+plt.xlim( [ -0.005, 0.08 ] )
 
-plt.text( 0.058, 0.0485, 'Fit Slope = ' '%s' r'$\, \pm \,$' '%s'
-%( round( m1, 2 ), round( mean( dat1_m ), 2 ) ), fontsize = 24, rotation=30 )
+plt.xticks([0, 0.015, 0.03, 0.045, 0.06, 0.075], fontsize=24 )
+plt.yticks([0, 0.015, 0.03, 0.045, 0.06, 0.075], fontsize=24 )
 
-plt.text( 0.052, 0.066, 'Expected Slope = 1', fontsize = 24, rotation=42 )
+plt.text( 0.055, 0.0493, 'Fit Slope = ' '%s' r'$\, \pm \,$' '%s'
+%( round( m1, 2 ), round( mean( dat1_m ), 2 ) ), fontsize = 20, rotation=33 )
 
+plt.text( 0.052, 0.069, 'Expected Slope = 1', fontsize = 20, rotation=45 )
+
+#plt.arrow(0, 0.075, 0.001, 0.003, head_width=0.001, head_length=0.002, fc='k', ec='k')
+#plt.arrow( 0.075, 0.0, 0.001, (0.0001-0) , shape='full', lw=0,
+#length_includes_head=True, head_width=.0015)
 #leg1 = [ 'Linear Fit', 'Theoretical Data', 'Observations' ]
 #plt.legend( leg1, loc = 2, fontsize = 30 )
 #plt.title(r'$\frac{\sigma_B}{| \vec B|}$ vs Fluctuating Velocity with error
 #bars', fontsize = 24 )
+
 rc( 'text', usetex=True )
 
 plt.xlabel( r'$\left< \delta B\right >/ B_0$', fontsize = 24 )
@@ -36,7 +45,7 @@ plt.ylabel( r'$\delta V/v_A$', fontsize = 24 )
 
 plt.tight_layout()
 
-plt.savefig('fv_delb_fz01'+'.eps', bbox_inches='tight', dpi=500)
+plt.savefig('fv_delb_fz01'+'.eps', bbox_inches='tight', dpi=200)
 
 #plt.show( )
 '''
